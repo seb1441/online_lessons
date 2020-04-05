e1957b257ae2a9631525b484afc67cbec7384b92
seb1441 <sebastienauriault@gmail.com>
2020-04-05 17:19:37 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get "/about", to: "pages#about", as: :pages_about
   get "/contact", to: "pages#contact", as: :pages_contact
-  get "/home", to: "pages#home", as: :pages_home
+  get "/", to: "pages#home", as: :pages_home
   get "/lessons", to: "pages#lessons", as: :pages_lessons
   get "/reviews", to: "pages#reviews", as: :pages_reviews
   get "/trial", to: "pages#trial", as: :pages_trial
